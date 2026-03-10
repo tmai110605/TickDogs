9f80cb46c7a7dd86f630ef41dc185eca8ba7f67f
_mqt. <23133071@student.hcmute.edu.vn>
2026-03-10 20:51:36 +0700

    Update TickNet_Dogs.py

--- TickNet_Dogs.py
+++ TickNet_Dogs.py
@@ -95,7 +95,7 @@ def get_data_loader(args, train):
     # Ép đường dẫn root='./data' (thư mục data nằm chung chỗ với file chạy)
     # Gán download=False để đọc trực tiếp dữ liệu offline
     # -------------------------------------------------------------------------
-    dataset = dataset_class(root='./data', train=train, download=False, transform=transform)
+    dataset = dataset_class(root='./data', train=train, download=True, transform=transform)
     
     return torch.utils.data.DataLoader(dataset, batch_size=args.batch_size, shuffle=True if train else False, num_workers=args.workers)
 
@@ -143,8 +143,8 @@ def run_epoch(train, data_loader, model, criterion, optimizer, n_epoch, args, de
             loss.backward()
             optimizer.step()
     
-        if (n_batch % 10) == 0:
-            print('[{}]  epoch {}/{},  batch {}/{},  loss_{}={:.5f},  acc_{}={:.2f}%'.format('train' if train else ' val ', n_epoch + 1, args.epochs, n_batch + 1, batch_count, "train" if train else "val", loss_item, "train" if train else "val", 100.0 * acc))
+        #if (n_batch % 10) == 0:
+           # print('[{}]  epoch {}/{},  batch {}/{},  loss_{}={:.5f},  acc_{}={:.2f}%'.format('train' if train else ' val ', n_epoch + 1, args.epochs, n_batch + 1, batch_count, "train" if train else "val", loss_item, "train" if train else "val", 100.0 * acc))
     
     return (sum(losses) / len(losses), sum(accs) / len(accs))
             
